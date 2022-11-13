acbdd89b1e82e39fd77b392e063d51369d095a0f
vla-gove <107414426+vla-gove@users.noreply.github.com>
2022-11-13 23:50:01 +0100

    Add files via upload

---
+++ TwoWheelieTransmitter.ino
@@ -0,0 +1,53 @@
+#include <VirtualWire.h>
+int forward = 1;
+int reverse = 2;
+int left = 3;
+int right = 4;
+int button;
+char temparray[5];
+
+void setup() {
+  vw_setup(2000);  
+  pinMode(forward, INPUT_PULLUP);
+  pinMode(reverse, INPUT_PULLUP);
+  pinMode(left, INPUT_PULLUP);
+  pinMode(right, INPUT_PULLUP);
+  Serial.begin(9600); 
+}
+
+void loop() {
+  
+  if (digitalRead(forward) == LOW) {
+    button = 200;
+    itoa(button, temparray, 10);
+
+    vw_send((uint8_t *)temparray, strlen(temparray));  
+    vw_wait_tx();                                              
+  }
+  
+  if (digitalRead(reverse) == LOW) {
+    button = 400;
+    itoa(button, temparray, 10);
+
+    vw_send((uint8_t *)temparray, strlen(temparray));  
+    vw_wait_tx();                                              
+  }
+
+  if (digitalRead(right) == LOW) {
+    button = 600;
+    itoa(button, temparray, 10);
+
+    vw_send((uint8_t *)temparray, strlen(temparray));  
+    vw_wait_tx();                                              
+  }
+
+  if (digitalRead(left) == LOW) {
+    button = 800;
+    itoa(button, temparray, 10);
+
+    vw_send((uint8_t *)temparray, strlen(temparray));  
+    vw_wait_tx();                                              
+  }
+
+  
+}
